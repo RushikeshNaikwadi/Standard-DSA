188c89ab8bca9092aaa88f09db0891c14b533a86
Rushikesh Naikwadi <i.rushi.naikwadi@gmail.com>
2022-11-30 23:21:51 +0530

    working on unorderd_map implementation

--- Hashing/1_unordered_map.cpp
+++ Hashing/1_unordered_map.cpp
@@ -1,6 +1,19 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+template <typename T>
+struct keyValNode {
+    string key;
+    T val;
+    keyValNode *next;
+
+    keyValNode (string inpKey, T inpVal) {
+        key = inpKey;
+        val = inpVal;
+        next = NULL;
+    }
+};
+
 int main () {
 
     return 0;
